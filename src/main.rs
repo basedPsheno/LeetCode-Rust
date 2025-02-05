d97a8f80fe680182c2cb3545b48d07aa6090f8ec
basedPsheno <based.psheno@gmail.com>
2025-02-05 22:29:45 +0300

    1790. Check if One String Swap Can Make Strings Equal

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,41 @@
 struct Solution;
 
 impl Solution{
-    pub fn foo() {
+    pub fn are_almost_equal(s1: String, s2: String) -> bool {
+        // по условию строки содержать только из строчных английских букв
+        let (mut buffer_char1, mut buffer_char2) = ('-', '-');
+        let mut diff_counter = 0;
 
+        for (c1, c2) in s1.chars().zip(s2.chars()) {
+            if c1 != c2 {
+                diff_counter += 1;
+                if diff_counter == 1 {
+                    buffer_char1 = c1;
+                    buffer_char2 = c2;
+                }
+                if diff_counter == 2 {
+                    if buffer_char1 != c2 || buffer_char2 != c1 {
+                        return false;
+                    }
+                }
+                if diff_counter > 2 {
+                    return false;
+                }
+            }
+        }
+
+        if diff_counter == 1 {
+            return false;
+        }
+        true
     }
 }
 
-fn main() {}
+fn main() {
+    // к следующим задачам сделаю полноценный тест-модуль
+    let s1 = "bank".to_string();
+    let s2 = "kanb".to_string();
+
+    let sol = Solution::are_almost_equal(s1, s2);
+    println!("{}", sol);
+}
