1bceb500e34e76f620efc544636aa7eb35883f7c
basedPsheno <based.psheno@gmail.com>
2025-02-11 17:34:55 +0300

    1910. Remove All Occurrences of a Substring

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,21 @@
 struct Solution;
 
 impl Solution {
-    pub fn clear_digits(s: String) -> String {
-        let mut char_pointer = Vec::new();
+    pub fn remove_occurrences(s: String, part: String) -> String {
         let n = s.len();
-        let mut check = vec![true; n];
-        let mut ans = Vec::new();
+        let m = part.len();
+        let part_chars: Vec<char> = part.chars().collect();
+        let mut stack = Vec::with_capacity(n);
 
-        for (i, c) in s.chars().enumerate() {
-            if c.is_ascii_digit() {
-                if let Some(ch) = char_pointer.pop() {
-                    check[ch] = false;
-                }
-                check[i] = false;
-            }
-
-            if c.is_ascii_alphabetic() {
-                char_pointer.push(i);
-            }
-
-        }
-
-        for (i, c) in s.chars().enumerate() {
-            if check[i] {
-                ans.push(c);
+        for c in s.chars() {
+            stack.push(c);
+            
+            if stack.len() >= m && stack[stack.len() - m..] == part_chars {
+                stack.truncate(stack.len() - m);
             }
         }
 
-        ans.into_iter().collect::<String>()
+        stack.into_iter().collect::<String>()
     }
 }
 
@@ -37,13 +25,13 @@ mod tests{
     use super::*;
 
     #[test]
-    fn clear_digits_example_1() {
-        assert_eq!(Solution::clear_digits("abc".to_string()), "abc".to_string());
+    fn remove_occurrences_example_1() {
+        assert_eq!(Solution::remove_occurrences("daabcbaabcbc".to_string(), "abc".to_string()), "dab".to_string());
     }
 
     #[test]
-    fn clear_digits_example_2() {
-        assert_eq!(Solution::clear_digits("cb34".to_string()), "".to_string());
+    fn remove_occurrences_example_2() {
+        assert_eq!(Solution::remove_occurrences("axxxxyyyyb".to_string(), "xy".to_string()), "ab".to_string());
     }
 }
 
