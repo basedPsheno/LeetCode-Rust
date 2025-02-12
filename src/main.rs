bad42833f875cea22d33dc2a9cc0e280677adf21
basedPsheno <based.psheno@gmail.com>
2025-02-12 13:12:33 +0300

    2342. Max Sum of a Pair With Equal Sum of Digits

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,51 @@
+use std::collections::HashMap;
+
 struct Solution;
 
 impl Solution {
-    pub fn remove_occurrences(s: String, part: String) -> String {
-        let n = s.len();
-        let m = part.len();
-        let part_chars: Vec<char> = part.chars().collect();
-        let mut stack = Vec::with_capacity(n);
+    pub fn maximum_sum(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut ans = -1;
+        let mut digits_sums: HashMap<i32, (i32, i32)> = HashMap::new();
 
-        for c in s.chars() {
-            stack.push(c);
-            
-            if stack.len() >= m && stack[stack.len() - m..] == part_chars {
-                stack.truncate(stack.len() - m);
+        for i in 0..n {
+            let mut sum = 0;
+            let mut num = nums[i];
+
+            while num > 0 {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            let num = nums[i];
+            if digits_sums.contains_key(&sum) {
+                let (a, b) = digits_sums[&sum];
+                let mut_digits_sum = digits_sums.get_mut(&sum).unwrap();
+                if b > 0 {
+                    if num > b {
+                        mut_digits_sum.1 = num;
+                        mut_digits_sum.0 = b;
+                    } else if num > a {
+                        mut_digits_sum.0 = num;
+                    }
+                } else {
+                    if num < a {
+                        mut_digits_sum.0 = num;
+                        mut_digits_sum.1 = a;
+                    } else {
+                        mut_digits_sum.1 = num;
+                    }
+                }
+
+                if mut_digits_sum.0 + mut_digits_sum.1 > ans {
+                    ans = mut_digits_sum.0 + mut_digits_sum.1;
+                }
+            } else {
+                digits_sums.insert(sum, (num, 0));
             }
         }
 
-        stack.into_iter().collect::<String>()
+        ans
     }
 }
 
@@ -25,13 +55,13 @@ mod tests{
     use super::*;
 
     #[test]
-    fn remove_occurrences_example_1() {
-        assert_eq!(Solution::remove_occurrences("daabcbaabcbc".to_string(), "abc".to_string()), "dab".to_string());
+    fn maximum_sum_example_1() {
+        assert_eq!(Solution::maximum_sum(vec![18,43,36,13,7]), 54);
     }
 
     #[test]
-    fn remove_occurrences_example_2() {
-        assert_eq!(Solution::remove_occurrences("axxxxyyyyb".to_string(), "xy".to_string()), "ab".to_string());
+    fn maximum_sum_example_2() {
+        assert_eq!(Solution::maximum_sum(vec![10,12,19,14]), -1);
     }
 }
 
