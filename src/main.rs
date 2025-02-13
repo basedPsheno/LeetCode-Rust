51be32505e6da9d8fb402ac0ffd42a04f2fa38eb
basedPsheno <based.psheno@gmail.com>
2025-02-13 17:51:27 +0300

    3066. Minimum Operations to Exceed Threshold Value II

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,55 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 struct Solution;
 
 impl Solution {
-    pub fn maximum_sum(nums: Vec<i32>) -> i32 {
-        let n = nums.len();
-        let mut ans = -1;
-        let mut digits_sums: HashMap<i32, (i32, i32)> = HashMap::new();
+    pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+        let mut btree = BTreeMap::new();
+        let mut ans = 0;
 
-        for i in 0..n {
-            let mut sum = 0;
-            let mut num = nums[i];
+        for num in nums {
+            *btree.entry(num as i64).or_insert(0) += 1;
+        }
 
-            while num > 0 {
-                sum += num % 10;
-                num /= 10;
+        println!("{:?}", btree);
+
+        while *btree.first_key_value().unwrap().0 < k as i64 {
+            println!("{:?}", btree);
+            let (mut min, mut max) = (-1, -1);
+            if let Some((x, count)) = btree.pop_first() {
+                if count > 1 {
+                    btree.insert(x, count - 1);
+                }
+                min = x;
+            }
+            if let Some((y, count)) = btree.pop_first() {
+                if count > 1 {
+                    btree.insert(y, count - 1);
+                }
+                max = y;
             }
 
-            let num = nums[i];
-            if digits_sums.contains_key(&sum) {
-                let (a, b) = digits_sums[&sum];
-                let mut_digits_sum = digits_sums.get_mut(&sum).unwrap();
-                if b > 0 {
-                    if num > b {
-                        mut_digits_sum.1 = num;
-                        mut_digits_sum.0 = b;
-                    } else if num > a {
-                        mut_digits_sum.0 = num;
-                    }
-                } else {
-                    if num < a {
-                        mut_digits_sum.0 = num;
-                        mut_digits_sum.1 = a;
-                    } else {
-                        mut_digits_sum.1 = num;
-                    }
-                }
+            btree.entry(min * 2 + max).and_modify(|v| *v += 1).or_insert(1);
 
-                if mut_digits_sum.0 + mut_digits_sum.1 > ans {
-                    ans = mut_digits_sum.0 + mut_digits_sum.1;
-                }
-            } else {
-                digits_sums.insert(sum, (num, 0));
-            }
+            ans += 1;
         }
 
         ans
     }
 }
 
-
 #[cfg(test)]
 mod tests{
     use super::*;
 
     #[test]
-    fn maximum_sum_example_1() {
-        assert_eq!(Solution::maximum_sum(vec![18,43,36,13,7]), 54);
+    fn min_operations_example_1() {
+        assert_eq!(Solution::min_operations(vec![2,11,10,1,3], 10), 2);
     }
 
     #[test]
-    fn maximum_sum_example_2() {
-        assert_eq!(Solution::maximum_sum(vec![10,12,19,14]), -1);
+    fn min_operations_example_2() {
+        assert_eq!(Solution::min_operations(vec![1,1,2,4,9], 20), 4);
     }
 }
 
