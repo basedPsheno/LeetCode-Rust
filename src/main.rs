00ae27fd7e67b48214dcbe35a79e4e74a32cf0a2
basedPsheno <based.psheno@gmail.com>
2025-02-17 17:19:38 +0300

    1079. Letter Tile Possibilities

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,50 @@
-use std::collections::HashSet;
+struct Solution;
 
-struct ProductOfNumbers {
-    prefix: Vec<i32>,
-}
-
-
-/** 
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
-impl ProductOfNumbers {
-    fn new() -> Self {
-        Self { prefix: vec![1] }
-    }
-
-    fn add(&mut self, num: i32) {
-        if num == 0 {
-            self.prefix = vec![1];
-        } else {
-            let last = *self.prefix.last().unwrap();
-            self.prefix.push(last * num);
+impl Solution {
+    fn num_tile_possibilities(tiles: String) -> i32 {
+        let mut freq = [0; 26];
+        for c in tiles.chars() {
+            freq[c as usize - 'A' as usize] += 1;
         }
-    }
-
-    fn get_product(&self, k: i32) -> i32 {
-        let k = k as usize;
-        let n = self.prefix.len();
-        if k >= n {
-            return 0;
+        
+        fn backtrack(freq: &mut [i32; 26], total: &mut i32) {
+            for i in 0..26 {
+                if freq[i] > 0 {
+                    freq[i] -= 1;
+                    *total += 1;
+                    
+                    let mut new_freq = *freq;
+                    backtrack(&mut new_freq, total);
+                    
+                    freq[i] += 1;
+                }
+            }
         }
-        self.prefix[n - 1] / self.prefix[n - 1 - k]
+        
+        let mut total = 0;
+        backtrack(&mut freq, &mut total);
+        total
     }
 }
-
-/**
- * Your ProductOfNumbers object will be instantiated and called as such:
- * let obj = ProductOfNumbers::new();
- * obj.add(num);
- * let ret_2: i32 = obj.get_product(k);
- */
 
 #[cfg(test)]
 mod tests{
     use super::*;
 
+    #[test]
+    fn num_tile_possibilities_example_1() {
+        assert_eq!(Solution::num_tile_possibilities("AAB".to_string()), 8);
+    }
+
+    #[test]
+    fn num_tile_possibilities_example_2() {
+        assert_eq!(Solution::num_tile_possibilities("AAABBC".to_string()), 188);
+    }
+
+    #[test]
+    fn num_tile_possibilities_example_3() {
+        assert_eq!(Solution::num_tile_possibilities("V".to_string()), 1);
+    }
 }
 
 fn main() {}
